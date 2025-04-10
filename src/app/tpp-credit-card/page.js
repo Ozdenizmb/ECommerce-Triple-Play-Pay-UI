055f62b57b3ccdeff3d6480ee2021b1c8a81f2fc
Mehmet Baran Özdeniz <ozdenizm6@gmail.com>
2025-04-10 14:45:01 +0300

    Tpp Kart Formu Tpp elementlerine bağımlı olmayacak hale getirildi

--- src/app/tpp-credit-card/page.js
+++ src/app/tpp-credit-card/page.js
@@ -1,19 +1,37 @@
 'use client';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const TppCreditCard = () => {
 
-    useEffect(() => {
-        var triple = new Triple(process.env.REACT_APP_TPP_PUBLIC_API_KEY);
+    const[token, setToken] = useState(null);
+    let triple;
 
+    useEffect(() => {
+        triple = new Triple(process.env.NEXT_PUBLIC_TPP_PUBLIC_API_KEY);
         triple.generatePaymentForm({
             containerSelector: '#example',
-            amount: 20.00,
+            paymentType: "charge",
+            description: "\"\"",
             paymentOptions: ['credit_card'],
-            phoneOption: false,
-            tokenMode: 'save',
+            saveMode: true,
             savePaymentToken: true,
-            optIn: true,
+            showEmail: false,
+            zipMode: "required",
+            fullName: false,
+            billingAddress: false,
+            optIn: false,
+            emailOption: false,
+            phoneOption: false,
+            showDescription: false,
+            hideSubmitButton: true,
+            isMin: true,
+            iframeMinHeightPx: 210,
+            isMobileResponsive: true,
+            hidePaymentDetailsLabel: true,
+
+            //googlePayEnvironment: 'TEST', // set this to test to evaluate Google Pay functionality locally
+            //googlePayMerchantId: '12345678901234567890',
+            
             //Handle error response
             onError: (errorData) => {
                 console.log('Error');
@@ -21,15 +39,25 @@ const TppCreditCard = () => {
             },
             // Handle success response
             onSuccess: (data) => {
+                setToken(data.payload.token)
                 console.log('Success');
-                console.log(data);
+                console.log("TOKEN: " + data.payload.token);
             },
         });
     }, []);
 
+    const onClickSaveCard = () => {
+        triple.tokenize();
+    }
+
     return (
         <div>
             <div id="example"></div>
+            <button className='btn btn-primary mt-5 text-center' onClick={onClickSaveCard}>SAVE CARD</button>
+            <div className='mb-5'>U Can Call Token Right Now. We Will Development This Project</div>
+            {
+                token !== null && <div className='mb-5'>Token: {token}</div>
+            }
         </div>
     );
 };
